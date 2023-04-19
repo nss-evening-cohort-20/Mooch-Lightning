c5184ed136344df7d34cadd87d1ce06c2d22f6d2
Jeremy White <jcwhite414@gmail.com>
2023-04-18 22:23:40 -0500

    Added controller, model, and repos for user

---
+++ Mooch-Lightning/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mooch_Lightning.Repositories;
+
+namespace Mooch_Lightning.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserController(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+}
---
+++ Mooch-Lightning/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Gifter.Repositories;
+using Mooch_Lightning.Model;
+
+namespace Mooch_Lightning.Repositories;
+
+public class UserRepository : BaseRepository, IUserRepository
+{
+    public UserRepository(IConfiguration configuration) : base(configuration) { }
+    public User GetById(int id) { return new User(); }
+}
