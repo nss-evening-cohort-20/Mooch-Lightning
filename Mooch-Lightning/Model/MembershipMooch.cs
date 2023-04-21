5d2a1d78cdc737188124db950927f1700ff7cfa7
r-stroud <stroudrobert86@gmail.com>
2023-04-20 21:54:13 -0500

    updated model properties

--- Mooch-Lightning/Model/MembershipMooch.cs
+++ Mooch-Lightning/Model/MembershipMooch.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public int UserId { get; set; }
+        public User User { get; set; }
         public int UserMembershipId { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
