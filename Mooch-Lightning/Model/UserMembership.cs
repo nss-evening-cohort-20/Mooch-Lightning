71d7b992ddca884dd22b1c513d123b03e92097fd
ChaseBurnett <chaseburnett@me.com>
2023-04-22 14:08:00 -0500

    Changed name to UserMemberRepository, added Post/Put/Delete Methods, Updated Controller, Updated Model, Needs to Be checked on Swagger

--- Mooch-Lightning/Model/UserMembership.cs
+++ Mooch-Lightning/Model/UserMembership.cs
@@ -6,8 +6,4 @@ public class UserMembership
     public int UserId { get; set; }
     public User User { get; set; }
     public int MembershipId { get; set; }
-    public bool IsMooched { get; set; }
-    public DateTime AvailabiltyStartDate { get; set; }
-    public DateTime AvailabiltyEndDate { get; set; }
-    
 }
--- Mooch-Lightning/Repositories/IUserMembershipRepository.cs
+++ Mooch-Lightning/Repositories/IUserMembershipRepository.cs
@@ -4,6 +4,9 @@ namespace Mooch_Lightning.Repositories
 {
     public interface IUserMembershipRepository
     {
+        UserMembership Add(UserMembership userMembership);
+        void Delete(int id);
         UserMembership GetById(int id);
+        UserMembership Update(UserMembership userMembership);
     }
 }
