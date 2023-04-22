8b0727fcc49f8279c0033d56388adc3cfadab00c
r-stroud <stroudrobert86@gmail.com>
2023-04-22 10:22:15 -0500

    Updated type properties to Models

--- Mooch-Lightning/Model/Organization.cs
+++ Mooch-Lightning/Model/Organization.cs
@@ -7,7 +7,11 @@ namespace Mooch_Lightning.Model
         public int Id { get; set; }
         public string Name { get; set; }
         public int OrganizationTypeId { get; set; }
+        public OrganizationType OrganizationType { get; set; }
         public string ImageUrl { get; set; }
+        public List<Location> Locations { get; set; }
+
+        // List of Memberships
     }
 }
 
