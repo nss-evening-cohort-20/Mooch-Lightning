5443c350d1132dcd8cc3d944dc6aedc56c5dbde2
crisneames <crisneames@gmail.com>
2023-04-21 19:28:59 -0500

    Membership model, controller, repository boilerplate

---
+++ Mooch-Lightning/Controllers/MembershipController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mooch_Lightning.Repositories;
+
+namespace Mooch_Lightning.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class MembershipController : ControllerBase
+{
+
+
+    private readonly IMembershipRepository _membershipRepository;
+
+    public MembershipController(IMembershipRepository membershipRepository)
+    {
+        _membershipRepository = membershipRepository;
+    }
+    // GET: Membership
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(_membershipRepository.GetAll());
+    }
+
+    // GET: Membership/Details/5
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var membership = _membershipRepository.GetById(id);
+        if (membership == null)
+        {
+            return NotFound();
+        }
+        return Ok(membership);
+    }
+}
---
+++ Mooch-Lightning/Model/Membership.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Mooch_Lightning.Model
+{
+    public class Membership
+
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int OrganizationId { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
+
