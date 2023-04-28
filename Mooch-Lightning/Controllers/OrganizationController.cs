c27331ba011626167d4e1bbec52438d00efd0e96
crisneames <crisneames@gmail.com>
2023-04-27 20:18:50 -0500

    Getting an organization by Id with their offered memberships

--- Mooch-Lightning/Controllers/OrganizationController.cs
+++ Mooch-Lightning/Controllers/OrganizationController.cs
@@ -11,101 +11,39 @@ namespace Mooch_Lightning.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 
-    public class OrganizationController : ControllerBase
+public class OrganizationController : ControllerBase
+{
+
+
+    private readonly IOrganizationRepository _organizationRepository;
+
+    public OrganizationController(IOrganizationRepository organizationRepository)
     {
-
-
-        private readonly IOrganizationRepository _organizationRepository;
-
-        public OrganizationController(IOrganizationRepository organizationRepository)
-        {
-            _organizationRepository = organizationRepository;
-        }
-        // GET: Organization
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok(_organizationRepository.GetAll());
-        }
-
-        // GET: Organization/Details/5
-        [HttpGet("{id}")]
-        public IActionResult Get(int id)
-        {
-            var organization = _organizationRepository.GetById(id);
-            if (organization == null)
-            {
-                return NotFound();
-            }
-            return Ok(organization);
-        }
-
-        // GET: Organization/Create
-       // public ActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        // POST: Organization/Create
-        //[HttpPost]
-       // [ValidateAntiForgeryToken]
-        //public ActionResult Create(IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add insert logic here
-
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        // GET: Organization/Edit/5
-        //public ActionResult Edit(int id)
-        //{
-        //    return View();
-        //}
-
-        // POST: Organization/Edit/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add update logic here
-
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        // GET: Organization/Delete/5
-        //public ActionResult Delete(int id)
-        //{
-        //    return View();
-        //}
-
-        // POST: Organization/Delete/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Delete(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add delete logic here
-
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
+        _organizationRepository = organizationRepository;
     }
+    // GET: Organization
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(_organizationRepository.GetAll());
+    }
+
+    // GET: Organization/Details/5
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var organization = _organizationRepository.GetById(id);
+        if (organization == null)
+        {
+            return NotFound();
+        }
+        return Ok(organization);
+    }
+
+    //Get: Organization with Membership
+    [HttpGet("withMembership/{Id}")]
+    public IActionResult GetOrganizationWithMemberships(int Id)
+    {
+        return Ok(_organizationRepository.GetOrganizationWithMembership(Id));
+    }
+}
--- Mooch-Lightning/Repositories/OrganizationRepository.cs
+++ Mooch-Lightning/Repositories/OrganizationRepository.cs
@@ -83,4 +83,66 @@ public class OrganizationRepository : BaseRepository, IOrganizationRepository
             }
         }
     }
+
+    public Organization GetOrganizationWithMembership(int Id)
+    {
+        using (var conn = Connection)
+        {
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"
+                SELECT  Organization.Id as OrganizationId
+                       ,Organization.Name
+                       ,Membership.Id as MembershipId
+                       ,Membership.Description
+                       ,Membership.ImageUrl
+                FROM Membership 
+                JOIN Organization on Membership.OrganizationId = Organization.Id
+                WHERE Organization.Id = @Id";
+
+                DbUtils.AddParameter(cmd, "@Id", Id);
+
+                var reader = cmd.ExecuteReader();
+
+                Organization organization = null;
+                while (reader.Read())
+                {
+                   
+
+                    if (organization == null)
+                    {
+                        organization = new Organization()
+                        {
+
+                            Id = Id,
+                            Name = DbUtils.GetString(reader, "Name"),
+
+
+                            Memberships = new List<Membership>()
+                        };
+
+                    };
+
+                    if (DbUtils.IsNotDbNull(reader, "MembershipId"))
+                    {
+                        organization.Memberships.Add(new Membership()
+                        {
+                            Id = DbUtils.GetInt(reader, "MembershipId"),
+                            OrganizationId = DbUtils.GetInt(reader, "OrganizationId"),
+                            Description = DbUtils.GetString(reader, "Description"),
+                            ImageUrl = DbUtils.GetString(reader, "ImageUrl")
+
+                        });
+                    }
+
+                   
+                }
+                reader.Close();
+
+                return organization;
+            }
+           
+        }
+    }
 }
