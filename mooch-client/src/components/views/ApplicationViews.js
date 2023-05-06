636edff28031c7b4e2f13da4f0cc643b6d8b2220
crisneames <crisneames@gmail.com>
2023-05-06 11:28:31 -0500

    create mooch post view and add css for it

--- mooch-client/src/components/views/ApplicationViews.js
+++ mooch-client/src/components/views/ApplicationViews.js
@@ -1,15 +1,14 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { CreateAccount } from "./CreateAccountView/CreateAccount";
-import { HomeView } from "./HomeView/HomeView";
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { CreateAccount } from './CreateAccountView/CreateAccount';
+import { HomeView } from './HomeView/HomeView';
+import { CreateMoochPostView } from './CreateMoochPostView/CreateMoochPostView';
 
 export const ApplicationViews = () => {
-
   return (
     <Routes>
-
       <Route path="/" element={<HomeView />} />
       <Route path="createUser" element={<CreateAccount />} />
-
+      <Route path="createMoochPostView" element={<CreateMoochPostView />} />
     </Routes>
   );
 };
---
+++ mooch-client/src/components/views/CreateMoochPostView/CreateMoochPostView.js
@@ -0,0 +1,108 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ROUTE_CONSTANTS } from '../../Utils/Constants';
+import { useParams } from 'react-router-dom';
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
+import './CreateMoochPostView.css';
+import { getCurrentUser } from '../../Utils/Constants';
+
+export const CreateMoochPostView = () => {
+  const [moochId, setMoochId] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [membershipArray, setMembershipArray] = useState([]);
+
+  const navigate = useNavigate();
+
+  const currentUser = getCurrentUser();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `${ROUTE_CONSTANTS.API_URL}/User/usermembershiplist/${currentUser.id}`
+      );
+      const membershipArray = await response.json();
+      setMembershipArray(membershipArray.memberships);
+    };
+    fetchData();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const membershipPost = {
+      UserMembershipId: moochId,
+      isMooched: false,
+      availabilityStartDate: startDate,
+      AvailabilityendDate: endDate,
+    };
+
+    fetch(`${ROUTE_CONSTANTS.API_URL}/MoochPost`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(membershipPost),
+    }).then(() => {
+      navigate('/');
+    });
+  };
+
+  return (
+    <Container className="form-align">
+      <div className="App">
+        <label>
+          <span
+            style={{ textAlign: 'left', fontSize: '25px', marginLeft: '15px' }}
+          >
+            Share a Membership For Mooching
+          </span>
+        </label>
+        <Form className="form" onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label for="select">Select</Label>
+            <Input
+              id="select"
+              name="Mooch Request"
+              type="select"
+              onChange={(e) => setMoochId(e.target.value)}
+            >
+              <option value="">-- Choose --</option>
+              {membershipArray.map((m) => {
+                return (
+                  <option key={m.id} value={m.id}>
+                    {m.organization + ' ' + '-' + ' ' + m.description}
+                  </option>
+                );
+              })}
+            </Input>
+          </FormGroup>
+          <div className="d-flex justify-content-around">
+            <FormGroup>
+              <Label for="startDate">Start Date</Label>
+              <Input
+                id="startDate"
+                name="date"
+                type="date"
+                placeholder="date placeholder"
+                defaultValue={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="endDate">End Date</Label>
+              <Input
+                id="endDate"
+                name="date"
+                placeholder="date placeholder"
+                type="date"
+                defaultValue={endDate}
+                onChange={(e) => setEndDate(e.target.value)}
+              />
+            </FormGroup>
+          </div>
+          <Button className="Btn">Submit</Button>
+        </Form>
+      </div>
+    </Container>
+  );
+};
